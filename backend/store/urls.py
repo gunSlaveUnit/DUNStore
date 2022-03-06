2c2b88ee7a360043f8bd6c6e99b4168e55d9b45d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-06 17:56:25 +0300

    Added an update processor url

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from store.views import Processors, ProcessorCreate, ProcessorDetail
+from store.views import Processors, ProcessorCreate, ProcessorDetail, ProcessorUpdate, ProcessorDelete
 
 urlpatterns = [
     path('processors/', Processors.as_view(), name='processors'),
     path('processors/add/', ProcessorCreate.as_view(), name='processor_create'),
     path('processors/<slug:slug>', ProcessorDetail.as_view(), name='processor_detail'),
+    path('processors/<slug:slug>/update', ProcessorUpdate.as_view(), name='processor_update'),
 ]
