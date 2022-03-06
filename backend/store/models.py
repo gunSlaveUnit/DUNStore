3f593b36bd03dfc54fb3cd0cece2650a7a6d2d18
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-06 13:44:23 +0300

    Added 2 fields in Product model: slug and price

--- backend/store/models.py
+++ backend/store/models.py
@@ -9,6 +9,8 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    price = models.IntegerField()
     is_published = models.BooleanField(default=True)
 
     @abstractmethod
