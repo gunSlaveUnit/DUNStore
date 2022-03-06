bf8f45096b8e263e4c060bae4a61f93a60a100e1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-06 13:53:15 +0300

    Added url for processor create view

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from store.views import Processors
+from store.views import Processors, ProcessorCreate
 
 urlpatterns = [
-    path('processors/', Processors.as_view(), name='processors')
+    path('processors/', Processors.as_view(), name='processors'),
+    path('processors/<slug:slug>', ProcessorCreate.as_view(), name='processor_create'),
 ]
