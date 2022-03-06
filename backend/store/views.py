dbf571d7b9bcf014e880491854f779ad5fb8e9f1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-06 13:52:11 +0300

    Added processor create view

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 from store.models import Processor
 
@@ -7,3 +7,8 @@ class Processors(ListView):
     model = Processor
     queryset = Processor.objects.filter(is_published=True)
     context_object_name = 'processors'
+
+
+class ProcessorCreate(CreateView):
+    model = Processor
+    fields = '__all__'
