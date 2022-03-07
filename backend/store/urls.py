c90235fb95f8eda6d263b0209beeb71934871fd9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-07 14:15:39 +0300

    Fixed view in url address

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from store.views import ProcessorsAPIView, ProcessorCreate, ProcessorDetail, ProcessorUpdate, ProcessorDelete
 
 urlpatterns = [
-    path('processors/', ProcessorsAPIView, name='processors'),
+    path('processors/', ProcessorsAPIView.as_view(), name='processors'),
     path('processors/add/', ProcessorCreate.as_view(), name='processor_create'),
     path('processors/<slug:slug>', ProcessorDetail.as_view(), name='processor_detail'),
     path('processors/<slug:slug>/update', ProcessorUpdate.as_view(), name='processor_update'),
