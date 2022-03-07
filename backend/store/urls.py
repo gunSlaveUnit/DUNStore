7e76bb800ae2feca1a1ed9a8a7e23964ac5f27f3
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-07 17:36:42 +0300

    Changed detail and delete views

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from store.views import ProcessorsAPIView, ProcessorCreateAPIView, ProcessorDetail, ProcessorUpdateAPIView, ProcessorDelete
+from store.views import ProcessorsAPIView, ProcessorCreateAPIView, ProcessorDetailAPIView, \
+    ProcessorUpdateAPIView, ProcessorDeleteAPIView
 
 urlpatterns = [
     path('processors/', ProcessorsAPIView.as_view(), name='processors'),
     path('processors/add/', ProcessorCreateAPIView.as_view(), name='processor_create'),
-    path('processors/<slug:slug>', ProcessorDetail.as_view(), name='processor_detail'),
+    path('processors/<slug:slug>', ProcessorDetailAPIView.as_view(), name='processor_detail'),
     path('processors/<slug:slug>/update', ProcessorUpdateAPIView.as_view(), name='processor_update'),
-    path('processors/<slug:slug>/delete', ProcessorDelete.as_view(), name='processor_delete'),
+    path('processors/<slug:slug>/delete', ProcessorDeleteAPIView.as_view(), name='processor_delete'),
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,6 +1,4 @@
-from django.urls import reverse_lazy
-from django.views.generic import DetailView, DeleteView
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
 from store.models import Processor
 from store.serializers import ProcessorSerializer
@@ -18,10 +16,11 @@ class ProcessorCreateAPIView(CreateAPIView):
     serializer_class = ProcessorSerializer
 
 
-class ProcessorDetail(DetailView):
+class ProcessorDetailAPIView(RetrieveAPIView):
     model = Processor
-    slug_field = 'slug'
-    context_object_name = 'processor'
+    lookup_field = 'slug'
+    queryset = Processor.objects.all()
+    serializer_class = ProcessorSerializer
 
 
 class ProcessorUpdateAPIView(UpdateAPIView):
@@ -31,8 +30,8 @@ class ProcessorUpdateAPIView(UpdateAPIView):
     lookup_field = 'slug'
 
 
-class ProcessorDelete(DeleteView):
+class ProcessorDeleteAPIView(DestroyAPIView):
     model = Processor
-    fields = '__all__'
-    context_object_name = 'processor'
-    success_url = reverse_lazy('processors')
+    serializer_class = ProcessorSerializer
+    queryset = Processor.objects.all()
+    lookup_field = 'slug'
