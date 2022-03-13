f24d1728f4600eebef433d87061b2b156e50a4d9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-13 22:01:41 +0300

    Added Content and Header test components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Content from "./Content";
 import Footer from "./Footer";
 
 export default class App extends React.Component {
     render() {
         return (
             <div className="App">
+                <Header/>
+                <Content/>
                 <Footer/>
             </div>
         );
---
+++ frontend/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>Sign In</li>
+                    <li>Sign Up</li>
+                </ul>
+            </div>
+        );
+    }
+}
