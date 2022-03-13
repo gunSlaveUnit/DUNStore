aff3f5aebc8ac1d2694a9625a4fc71792cfe7852
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-13 22:26:30 +0300

    Added fetch list of processors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
         return (
             <div className="App">
                 <Header/>
-                <Content/>
+                <Content processors/>
                 <Footer/>
             </div>
         );
--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
+function fetchProducts(productsListName) {
+    console.log(`http://localhost:8000/api/v1/store/${productsListName}`)
+    return fetch(`http://localhost:8000/api/v1/store/${productsListName}`)
+}
+
 export default class Content extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        }
+    }
+
+    componentDidMount() {
+        fetchProducts(this.props.toString()).then(response => {
+            this.setState({
+                products: response.products
+            })
+        })
+        console.log(this.products)
+    }
+
     render() {
         return (
             <div>
