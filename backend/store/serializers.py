8028f4caeb7d12fa74828c8ddd70e71fc8173b50
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-20 20:47:11 +0300

    Added Supply Type serializer

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from store.models import Processor
+from store.models import Processor, SupplyType
 
 
 class ProcessorSerializer(ModelSerializer):
     class Meta:
         model = Processor
+        fields = '__all__'
+
+
+class SupplyTypeSerializer(ModelSerializer):
+    class Meta:
+        model = SupplyType
         fields = '__all__'
