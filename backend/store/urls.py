352e4fb9129697e3e3cf5d201950cf4a33ba313e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-20 20:49:24 +0300

    Added supply_types url

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from store.views import ProcessorsAPIView, ProcessorCreateAPIView, ProcessorDetailAPIView, \
-    ProcessorUpdateAPIView, ProcessorDeleteAPIView
+    ProcessorUpdateAPIView, ProcessorDeleteAPIView, \
+    SupplyTypeAPIView, SupplyTypeCreateAPIView, SupplyTypeDetailAPIView, SupplyTypeUpdateAPIView, SupplyTypeDeleteAPIView
 
 urlpatterns = [
     path('processors/', ProcessorsAPIView.as_view(), name='processors'),
@@ -9,4 +10,10 @@ urlpatterns = [
     path('processors/<slug:slug>', ProcessorDetailAPIView.as_view(), name='processor_detail'),
     path('processors/<slug:slug>/update', ProcessorUpdateAPIView.as_view(), name='processor_update'),
     path('processors/<slug:slug>/delete', ProcessorDeleteAPIView.as_view(), name='processor_delete'),
+
+    path('supply_types/', SupplyTypeAPIView.as_view(), name='supply_types'),
+    path('supply_types/add/', SupplyTypeCreateAPIView.as_view(), name='supply_type_create'),
+    path('supply_types/<slug:slug>', SupplyTypeDetailAPIView.as_view(), name='supply_type_detail'),
+    path('supply_types/<slug:slug>/update', SupplyTypeUpdateAPIView.as_view(), name='supply_type_update'),
+    path('supply_types/<slug:slug>/delete', SupplyTypeDeleteAPIView.as_view(), name='supply_type_delete'),
 ]
