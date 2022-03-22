6f330e5d18c5005a5768f8394e8822d476671cd1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-22 15:41:00 +0300

    Urls are made of the same type. Missing slashes added for this

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
 
     path('supply_types/', SupplyTypeAPIView.as_view(), name='supply_types'),
     path('supply_types/add/', SupplyTypeCreateAPIView.as_view(), name='supply_type_create'),
-    path('supply_types/<slug:slug>', SupplyTypeDetailAPIView.as_view(), name='supply_type_detail'),
-    path('supply_types/<slug:slug>/update', SupplyTypeUpdateAPIView.as_view(), name='supply_type_update'),
-    path('supply_types/<slug:slug>/delete', SupplyTypeDeleteAPIView.as_view(), name='supply_type_delete'),
+    path('supply_types/<slug:slug>/', SupplyTypeDetailAPIView.as_view(), name='supply_type_detail'),
+    path('supply_types/<slug:slug>/update/', SupplyTypeUpdateAPIView.as_view(), name='supply_type_update'),
+    path('supply_types/<slug:slug>/delete/', SupplyTypeDeleteAPIView.as_view(), name='supply_type_delete'),
 ]
