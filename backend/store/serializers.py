2a11c33cee91104c80d53068b7feb5b4e59ec655
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-22 15:43:39 +0300

    Added a to_internal_value method for the correct operation of the serializer

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 from store.models import Processor, SupplyType
 
@@ -6,7 +6,7 @@ from store.models import Processor, SupplyType
 class SupplyTypeSerializer(ModelSerializer):
     class Meta:
         model = SupplyType
-        fields = ('id', 'title', 'slug')
+        fields = '__all__'
 
 
 class ProcessorSerializer(ModelSerializer):
@@ -15,3 +15,15 @@ class ProcessorSerializer(ModelSerializer):
     class Meta:
         model = Processor
         fields = '__all__'
+
+    def to_internal_value(self, data):
+        supply_type_id = data.get('supply_type')
+        internal_data = super().to_internal_value(data)
+        try:
+            supply_type = SupplyType.objects.get(id=supply_type_id)
+        except SupplyType.DoesNotExist:
+            raise ValidationError(
+                {'supply_type': ['Item does not exist']},
+            )
+        internal_data['supply_type'] = supply_type
+        return internal_data
