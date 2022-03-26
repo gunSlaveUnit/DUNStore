430ab8d74a07c7a5ca244d9ad16c6af85c38d134
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-26 21:10:37 +0300

    Added simple router for existing entities

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,17 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from store.views import ProcessorViewSet, SupplyTypeViewSet
 
+router = routers.SimpleRouter()
+router.register(r'processor', ProcessorViewSet)
+router.register(r'supplyType', SupplyTypeViewSet)
+
 urlpatterns = [
-    path('processors/', ProcessorViewSet.as_view({'get': 'list'}), name='processors'),
-    # path('processors/add/', ProcessorViewSet.as_view(), name='processor_create'),
-    # path('processors/<slug:slug>', ProcessorViewSet.as_view(), name='processor_detail'),
-    # path('processors/<slug:slug>/update', ProcessorViewSet.as_view(), name='processor_update'),
-    # path('processors/<slug:slug>/delete', ProcessorViewSet.as_view(), name='processor_delete'),
-    #
-    # path('supply_types/', SupplyTypeViewSet.as_view(), name='supply_types'),
-    # path('supply_types/add/', SupplyTypeViewSet.as_view(), name='supply_type_create'),
-    # path('supply_types/<slug:slug>/', SupplyTypeViewSet.as_view(), name='supply_type_detail'),
-    # path('supply_types/<slug:slug>/update/', SupplyTypeViewSet.as_view(), name='supply_type_update'),
-    # path('supply_types/<slug:slug>/delete/', SupplyTypeViewSet.as_view(), name='supply_type_delete'),
+    path('', include(router.urls)),
 ]
