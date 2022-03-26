011ca168c48d068d2a8eb317a628808d8959a8eb
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-26 20:54:46 +0300

    All views are viewsets now

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,19 +1,17 @@
 from django.urls import path
 
-from store.views import ProcessorsAPIView, ProcessorCreateAPIView, ProcessorDetailAPIView, \
-    ProcessorUpdateAPIView, ProcessorDeleteAPIView, \
-    SupplyTypeAPIView, SupplyTypeCreateAPIView, SupplyTypeDetailAPIView, SupplyTypeUpdateAPIView, SupplyTypeDeleteAPIView
+from store.views import ProcessorViewSet, SupplyTypeViewSet
 
 urlpatterns = [
-    path('processors/', ProcessorsAPIView.as_view(), name='processors'),
-    path('processors/add/', ProcessorCreateAPIView.as_view(), name='processor_create'),
-    path('processors/<slug:slug>', ProcessorDetailAPIView.as_view(), name='processor_detail'),
-    path('processors/<slug:slug>/update', ProcessorUpdateAPIView.as_view(), name='processor_update'),
-    path('processors/<slug:slug>/delete', ProcessorDeleteAPIView.as_view(), name='processor_delete'),
-
-    path('supply_types/', SupplyTypeAPIView.as_view(), name='supply_types'),
-    path('supply_types/add/', SupplyTypeCreateAPIView.as_view(), name='supply_type_create'),
-    path('supply_types/<slug:slug>/', SupplyTypeDetailAPIView.as_view(), name='supply_type_detail'),
-    path('supply_types/<slug:slug>/update/', SupplyTypeUpdateAPIView.as_view(), name='supply_type_update'),
-    path('supply_types/<slug:slug>/delete/', SupplyTypeDeleteAPIView.as_view(), name='supply_type_delete'),
+    path('processors/', ProcessorViewSet.as_view({'get': 'list'}), name='processors'),
+    # path('processors/add/', ProcessorViewSet.as_view(), name='processor_create'),
+    # path('processors/<slug:slug>', ProcessorViewSet.as_view(), name='processor_detail'),
+    # path('processors/<slug:slug>/update', ProcessorViewSet.as_view(), name='processor_update'),
+    # path('processors/<slug:slug>/delete', ProcessorViewSet.as_view(), name='processor_delete'),
+    #
+    # path('supply_types/', SupplyTypeViewSet.as_view(), name='supply_types'),
+    # path('supply_types/add/', SupplyTypeViewSet.as_view(), name='supply_type_create'),
+    # path('supply_types/<slug:slug>/', SupplyTypeViewSet.as_view(), name='supply_type_detail'),
+    # path('supply_types/<slug:slug>/update/', SupplyTypeViewSet.as_view(), name='supply_type_update'),
+    # path('supply_types/<slug:slug>/delete/', SupplyTypeViewSet.as_view(), name='supply_type_delete'),
 ]
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,70 +1,16 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 from store.models import Processor, SupplyType
 from store.serializers import ProcessorSerializer, SupplyTypeSerializer
 
 
-class ProcessorsAPIView(ListAPIView):
-    model = Processor
+class ProcessorViewSet(ModelViewSet):
     queryset = Processor.objects.filter(is_published=True)
-    context_object_name = 'processors'
     serializer_class = ProcessorSerializer
-
-
-class ProcessorCreateAPIView(CreateAPIView):
-    model = Processor
-    serializer_class = ProcessorSerializer
-
-
-class ProcessorDetailAPIView(RetrieveAPIView):
-    model = Processor
-    lookup_field = 'slug'
-    queryset = Processor.objects.all()
-    serializer_class = ProcessorSerializer
-
-
-class ProcessorUpdateAPIView(UpdateAPIView):
-    model = Processor
-    serializer_class = ProcessorSerializer
-    queryset = Processor.objects.all()
     lookup_field = 'slug'
 
 
-class ProcessorDeleteAPIView(DestroyAPIView):
-    model = Processor
-    serializer_class = ProcessorSerializer
-    queryset = Processor.objects.all()
-    lookup_field = 'slug'
-
-
-class SupplyTypeAPIView(ListAPIView):
-    model = SupplyType
-    queryset = SupplyType.objects.all()
-    context_object_name = 'supply_types'
-    serializer_class = SupplyTypeSerializer
-
-
-class SupplyTypeCreateAPIView(CreateAPIView):
-    model = SupplyType
-    serializer_class = SupplyTypeSerializer
-
-
-class SupplyTypeDetailAPIView(RetrieveAPIView):
-    model = SupplyType
-    lookup_field = 'slug'
+class SupplyTypeViewSet(ModelViewSet):
     queryset = SupplyType.objects.all()
     serializer_class = SupplyTypeSerializer
-
-
-class SupplyTypeUpdateAPIView(UpdateAPIView):
-    model = SupplyType
-    serializer_class = SupplyTypeSerializer
-    queryset = SupplyType.objects.all()
-    lookup_field = 'slug'
-
-
-class SupplyTypeDeleteAPIView(DestroyAPIView):
-    model = SupplyType
-    serializer_class = SupplyTypeSerializer
-    queryset = SupplyType.objects.all()
     lookup_field = 'slug'
