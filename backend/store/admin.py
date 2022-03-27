ea371ff738487715efca2d5fbd957dacda5ac4a4
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-27 13:36:51 +0300

    Models in admin panel registered without class form

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -3,39 +3,10 @@ from django.contrib import admin
 from store.models import Processor, SupplyType, Socket, Chipset, RAMType, RAMFrequency, RAMGeneration
 
 
-# TODO: make admin.register(model_name), not classes
-
-
-@admin.register(Processor)
-class ProcessorAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(SupplyType)
-class SupplyTypeAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Socket)
-class SocketAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Chipset)
-class ChipsetAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(RAMType)
-class RAMTypeAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(RAMFrequency)
-class RAMFrequencyAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(RAMGeneration)
-class RAMGenerationAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(Processor)
+admin.site.register(SupplyType)
+admin.site.register(Socket)
+admin.site.register(Chipset)
+admin.site.register(RAMType)
+admin.site.register(RAMFrequency)
+admin.site.register(RAMGeneration)
