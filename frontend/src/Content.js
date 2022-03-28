7c617d790e11f33e85ba9ee0959bbe318ffc6f44
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-28 16:19:44 +0300

    Trying to make a content page

--- frontend/src/Content.js
+++ frontend/src/Content.js
@@ -2,11 +2,36 @@ import React from 'react';
 import ProductCard from "./ProductCard";
 
 export default class Content extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: []
+        }
+    }
+
+    fetchContent() {
+        return fetch("http://localhost:8000/api/v1/store/sockets/", {method: 'GET'})
+    }
+
+    componentDidMount() {
+        this.fetchContent().then(response => {
+            this.setState({
+                cards: response.cards
+            });
+        });
+
+        console.log(this.cards)
+
+        this.state.cards = this.cards.map((c) =>
+            <ProductCard card = c/>
+        );
+    }
+
     render() {
         return (
             <main>
                 <article>
-
+                    <ul>{cards}</ul>
                 </article>
             </main>
         );
