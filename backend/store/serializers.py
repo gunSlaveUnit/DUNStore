653c6a51518b4ae6ce6a47e1fcf5dacc736d6724
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-03-29 16:24:56 +0300

    Added warranty field to the serializer

--- backend/store/serializers.py
+++ backend/store/serializers.py
@@ -64,6 +64,7 @@ class RAMSerializer(ModelSerializer):
     mode = RAMWorkingModeSerializer(read_only=True)
     frequency = RAMFrequencySerializer(read_only=True)
     latency = RAMLatencySerializer(read_only=True)
+    warranty_period = PeriodSerializer(read_only=True)
 
     class Meta:
         model = RAM
@@ -75,6 +76,7 @@ class RAMSerializer(ModelSerializer):
         mode_id = data.get('mode')
         frequency_id = data.get('frequency')
         latency_id = data.get('latency')
+        warranty_period_id = data.get('warranty_period')
         internal_data = super().to_internal_value(data)
         try:
             form_factor = RAMType.objects.get(id=form_factor_id)
@@ -82,6 +84,7 @@ class RAMSerializer(ModelSerializer):
             mode = RAMWorkingMode.objects.get(id=mode_id)
             frequency = RAMFrequency.objects.get(id=frequency_id)
             latency = RAMLatency.objects.get(id=latency_id)
+            warranty_period = Period.objects.get(id=warranty_period_id)
         except RAMType.DoesNotExist:
             raise ValidationError(
                 {'form_factor': ['Item does not exist']},
@@ -102,17 +105,24 @@ class RAMSerializer(ModelSerializer):
             raise ValidationError(
                 {'latency': ['Item does not exist']},
             )
+        except Period.DoesNotExist:
+            raise ValidationError(
+                {'warranty_period': ['Item does not exist']},
+            )
         internal_data['form_factor'] = form_factor
         internal_data['generation'] = generation
         internal_data['mode'] = mode
         internal_data['frequency'] = frequency
         internal_data['latency'] = latency
+        internal_data['warranty_period'] = warranty_period
+
         return internal_data
 
 
 class ProcessorSerializer(ModelSerializer):
     supply_type = SupplyTypeSerializer(read_only=True)
     socket = SocketSerializer(read_only=True)
+    warranty_period = PeriodSerializer(read_only=True)
 
     class Meta:
         model = Processor
@@ -121,10 +131,12 @@ class ProcessorSerializer(ModelSerializer):
     def to_internal_value(self, data):
         supply_type_id = data.get('supply_type')
         socket_id = data.get('socket')
+        warranty_period_id = data.get('warranty_period')
         internal_data = super().to_internal_value(data)
         try:
             supply_type = SupplyType.objects.get(id=supply_type_id)
             socket = Socket.objects.get(id=socket_id)
+            warranty_period = Period.objects.get(id=warranty_period_id)
         except SupplyType.DoesNotExist:
             raise ValidationError(
                 {'supply_type': ['Item does not exist']},
@@ -133,27 +145,64 @@ class ProcessorSerializer(ModelSerializer):
             raise ValidationError(
                 {'socket': ['Item does not exist']},
             )
+        except Period.DoesNotExist:
+            raise ValidationError(
+                {'warranty_period': ['Item does not exist']},
+            )
         internal_data['supply_type'] = supply_type
         internal_data['socket'] = socket
+        internal_data['warranty_period'] = warranty_period
+
         return internal_data
 
 
 class PowerUnitSerializer(ModelSerializer):
+    warranty_period = PeriodSerializer(read_only=True)
+
     class Meta:
         model = PowerUnit
         fields = '__all__'
 
+    def to_internal_value(self, data):
+        warranty_period_id = data.get('warranty_period')
+        internal_data = super().to_internal_value(data)
+        try:
+            warranty_period = Period.objects.get(id=warranty_period_id)
+        except Period.DoesNotExist:
+            raise ValidationError(
+                {'warranty_period': ['Item does not exist']},
+            )
+        internal_data['warranty_period'] = warranty_period
+
+        return internal_data
+
 
 class GraphicsCardSerializer(ModelSerializer):
+    warranty_period = PeriodSerializer(read_only=True)
+
     class Meta:
         model = GraphicsCard
         fields = '__all__'
+
+    def to_internal_value(self, data):
+        warranty_period_id = data.get('warranty_period')
+        internal_data = super().to_internal_value(data)
+        try:
+            warranty_period = Period.objects.get(id=warranty_period_id)
+        except Period.DoesNotExist:
+            raise ValidationError(
+                {'warranty_period': ['Item does not exist']},
+            )
+        internal_data['warranty_period'] = warranty_period
+
+        return internal_data
 
 
 class BuildSerializer(ModelSerializer):
     processor = ProcessorSerializer(read_only=True)
     ram_module = RAMSerializer(read_only=True)
     power_unit = PowerUnitSerializer(read_only=True)
+    warranty_period = PeriodSerializer(read_only=True)
 
     class Meta:
         model = Build
@@ -163,14 +212,20 @@ class BuildSerializer(ModelSerializer):
         processor_id = data.get('processor')
         ram_module_id = data.get('ram_module')
         power_unit_id = data.get('power_unit')
+        warranty_period_id = data.get('warranty_period')
         internal_data = super().to_internal_value(data)
         try:
             processor = Processor.objects.get(id=processor_id)
             ram_module = RAM.objects.get(id=ram_module_id)
             power_unit = PowerUnit.objects.get(id=power_unit_id)
+            warranty_period = Period.objects.get(id=warranty_period_id)
         except Processor.DoesNotExist:
             raise ValidationError(
                 {'processor': ['Item does not exist']},
+            )
+        except Period.DoesNotExist:
+            raise ValidationError(
+                {'warranty_period': ['Item does not exist']},
             )
         except RAM.DoesNotExist:
             raise ValidationError(
@@ -183,4 +238,6 @@ class BuildSerializer(ModelSerializer):
         internal_data['processor'] = processor
         internal_data['ram_module'] = ram_module
         internal_data['power_unit'] = power_unit
+        internal_data['warranty_period'] = warranty_period
+
         return internal_data
