f942f068293245d7dfef1ea78625c30249ba1ccd
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-02 17:56:15 +0300

    Refactoring

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -5,13 +5,14 @@ from store.views import BuildSerializer, \
     StorageDeviceSerializer, VideoCardSerializer, MotherboardSerializer, ProcessorViewSet, RAMViewSet, PowerUnitViewSet
 
 router = routers.SimpleRouter()
-router.register(r'builds', BuildSerializer)
-router.register(r'storage_devices', StorageDeviceSerializer)
-router.register(r'video_cards', VideoCardSerializer)
-router.register(r'motherboards', MotherboardSerializer)
+
 router.register(r'processors', ProcessorViewSet)
 router.register(r'rams', RAMViewSet)
 router.register(r'power_units', PowerUnitViewSet)
+router.register(r'motherboards', MotherboardSerializer)
+router.register(r'video_cards', VideoCardSerializer)
+router.register(r'storage_devices', StorageDeviceSerializer)
+router.register(r'builds', BuildSerializer)
 
 urlpatterns = [
     path('', include(router.urls)),
