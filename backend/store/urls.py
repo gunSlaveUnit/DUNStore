f13b2d1a5482167548fbfb3a3645c29fbeaa177e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-02 19:07:32 +0300

    Fixed problem with urls

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from store.views import BuildSerializer, \
-    StorageDeviceSerializer, VideoCardSerializer, MotherboardSerializer, ProcessorViewSet, RAMViewSet, PowerUnitViewSet
+from store.views import BuildViewSet, \
+    StorageDeviceViewSet, VideoCardViewSet, MotherboardViewSet, ProcessorViewSet, RAMViewSet, PowerUnitViewSet
 
 router = routers.SimpleRouter()
 
 router.register(r'processors', ProcessorViewSet)
 router.register(r'rams', RAMViewSet)
 router.register(r'power_units', PowerUnitViewSet)
-router.register(r'motherboards', MotherboardSerializer)
-router.register(r'video_cards', VideoCardSerializer)
-router.register(r'storage_devices', StorageDeviceSerializer)
-router.register(r'builds', BuildSerializer)
+router.register(r'motherboards', MotherboardViewSet)
+router.register(r'video_cards', VideoCardViewSet)
+router.register(r'storage_devices', StorageDeviceViewSet)
+router.register(r'builds', BuildViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
