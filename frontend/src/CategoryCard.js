dd6bd3518716a9ff833ec1502afa6c71c2c8ed69
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-03 12:08:11 +0300

    Changed href

--- frontend/src/CategoryCard.js
+++ frontend/src/CategoryCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function CategoryCard (props) {
     return (
          <section className="CategoryCard">
-             <a href={`/catalog/${props.slug}`}>
+             <a href={`/catalog/${props.slug}/list`}>
                 <h2>{props.title}</h2>
              </a>
          </section>
