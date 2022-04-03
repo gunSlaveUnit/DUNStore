d608c890c2e81fcd211ee0d0a4f82c8d63f19603
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-03 15:28:08 +0300

    Created a new Create component which can make a form for a product

---
+++ frontend/src/Pages/Create.js
@@ -0,0 +1,35 @@
+import React, {useEffect} from "react";
+import * as API from "../API";
+import Header from "../PageComponents/Header";
+import Footer from "../PageComponents/Footer";
+
+export default function Create({what, how}) {
+    const [product, setProduct] = React.useState([]);
+
+    useEffect( async () => {
+        setProduct(await API.create(what, how))
+    }, [how, what])
+
+    return (
+        <div className="Create">
+            <Header/>
+            <main>
+                <article>
+                    <section>
+                        <form>
+                            <h1>Create a new product</h1>
+                            {Object.keys(product).map(f =>
+                                <div className="field" key={f}>
+                                    <p>{f.charAt(0).toUpperCase() + f.slice(1)}</p>
+                                    <input type="text"/>
+                                </div>
+                            )}
+                            <button type={"submit"}>Submit</button>
+                        </form>
+                    </section>
+                </article>
+            </main>
+            <Footer/>
+        </div>
+    );
+}
