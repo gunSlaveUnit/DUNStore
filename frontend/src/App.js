9c9da0ad2c60ab97e941db9dd38ac3067e7fa9b1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-03 12:06:54 +0300

    Updated routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import {useRoutes} from 'hookrouter';
 import NotFoundPage from './NotFoundPage';
 import Home from "./Home";
 import Catalog from "./Catalog";
+import Detail from "./Detail";
 
 export default function App() {
     const routes = {
         '/': () => <Home/>,
-        '/catalog/:slug': ({slug}) => <Catalog what={slug}/>
+        '/catalog/:catalog/list': ({catalog}) => <Catalog what={catalog}/>,
+        '/catalog/:catalog/detail/:detail': ({catalog, detail}) => <Detail what={catalog} slug={detail}/>,
     }
 
     const match = useRoutes(routes);
