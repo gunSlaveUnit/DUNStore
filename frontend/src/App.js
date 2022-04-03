7e3ebce4e9a477c6420640deac6fae3cd9df6120
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-03 15:30:25 +0300

    Added route for create component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,13 @@ import NotFoundPage from './Pages/NotFoundPage';
 import Home from "./Pages/Home";
 import Catalog from "./Pages/Catalog";
 import Detail from "./Pages/Detail";
+import Create from "./Pages/Create";
 
 export default function App() {
     const routes = {
         '/': () => <Home/>,
         '/catalog/:catalog/list': ({catalog}) => <Catalog what={catalog}/>,
+        '/catalog/:catalog/create': ({catalog}) => <Create what={catalog} how={{}}/>,
         '/catalog/:catalog/detail/:detail': ({catalog, detail}) => <Detail what={catalog} slug={detail}/>,
     }
 
