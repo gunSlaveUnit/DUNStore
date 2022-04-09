be7c8ad1cb050d455bce503709d80e6d6735d7d4
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-10 00:10:14 +0300

    Trying to add css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as ReactDomClient from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './tests/reportWebVitals';
+import './assets/main.css';
 
 ReactDomClient
     .createRoot(document.getElementById('root'))
