35ac945393e93b2d4da611cef74d580a4ffc3e8b
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-10 00:14:07 +0300

    Footer: component -> function

---
+++ frontend/src/PageComponents/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <small>&copy; GURU {new Date().getFullYear()}. All rights reserved</small>
+        </footer>
+    );
+}
+
+export default Footer;
