2dbdfd72236d52b792965c0cd7df7a413a92a011
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-10 18:20:26 +0300

    Added conditional rendering of user buttons: login and register when user is not authenticated, and logout otherwise

--- frontend/src/PageComponents/Header.jsx
+++ frontend/src/PageComponents/Header.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import {A} from 'hookrouter';
-import * as USER_API from '../User';
+import SignInModal from "../SignInModal";
+import {signout} from "../User";
 
 const Header = () => {
+    const [token, setToken] = React.useState(null);
+    const tokenAPI = {token, setToken};
+
     return (
         <header>
             <nav>
                 <ul>
                     <li><A href="/">Home</A></li>
-                    <li><button onClick={() => USER_API.signin}>Sign In</button></li>
-                    <li><button onClick={() => USER_API.signup}>Sign Up</button></li>
-                    <li><button onClick={() => USER_API.signout}>Sign Out</button></li>
+
+                    {token === null &&
+                        <div>
+                            <li>
+                                <button onClick={() => {
+                                }}>Sign In
+                                </button>
+                            </li>
+                            <li>
+                                <button onClick={() => {
+                                }}>Sign Up
+                                </button>
+                            </li>
+                        </div>
+                    }
+
+                    {token !== null &&
+                        //TODO: we can make profile page here
+                        <div>
+                            <li>
+                                <button onClick={() => {
+                                    signout(token).then(_ => tokenAPI.setToken(null))
+                                }}>Sign Out
+                                </button>
+                            </li>
+                        </div>
+                    }
+
                 </ul>
             </nav>
+
+            <SignInModal tokenAPI={tokenAPI}/>
         </header>
     );
 }
