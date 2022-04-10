bacc13395bce24a7465724f3ff4216081a38f9d9
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-10 15:13:17 +0300

    Removed debug token

--- frontend/src/PageComponents/Header.jsx
+++ frontend/src/PageComponents/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                     <li><A href="/">Home</A></li>
                     <li><button onClick={() => USER_API.signin}>Sign In</button></li>
                     <li><button onClick={() => USER_API.signup}>Sign Up</button></li>
-                    <li><button onClick={() => USER_API.signout('ed0a337c4840504d542b5b50d56fdd9762b12293')}>Sign Out</button></li>
+                    <li><button onClick={() => USER_API.signout}>Sign Out</button></li>
                 </ul>
             </nav>
         </header>
