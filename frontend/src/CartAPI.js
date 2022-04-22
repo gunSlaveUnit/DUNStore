6ca028ee0a5fc741b514b706ca62566017dc60b0
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-22 23:15:33 +0300

    Added a react-cookie package. Token can be stored in cookie. Added test "add" method to a shopping cart

---
+++ frontend/src/CartAPI.js
@@ -0,0 +1,7 @@
+const URL_BASE = "http://localhost:8000/api/v1/store/cart/";
+
+export async function add(token, category, slug) {
+   console.log(token)
+   console.log(category)
+   console.log(slug)
+}
--- frontend/src/PageComponents/Header.jsx
+++ frontend/src/PageComponents/Header.jsx
@@ -4,10 +4,15 @@ import {A, navigate} from 'hookrouter';
 import DeleteIcon from "@mui/icons-material/Delete";
 import * as API from "../API";
 import {ShoppingCart} from "@mui/icons-material";
+import {useCookies} from "react-cookie";
+import {signin} from "../User";
 
 const Header = () => {
-    const [token, setToken] = React.useState(null);
-    const tokenAPI = {token, setToken};
+    const [cookies, setCookie, removeCookie] = useCookies(['token']);
+    const tokenAPI = {
+        token: () => cookies.token,
+        setToken: token => setCookie('token', token),
+    };
 
     return (
         <AppBar position="fixed" style={{ background: 'transparent', boxShadow: 'none'}}>
@@ -20,7 +25,10 @@ const Header = () => {
                     ><A href={'/'} style={{textDecoration: 'none', color:"whitesmoke"}}>GURU</A></Typography>
                     <Box mr={3}>
                         <Button color={"inherit"} variant={"outlined"} mr={1} size={"large"}
-                                sx={{borderRadius: '8px', marginX: 3}}>Sign In</Button>
+                                sx={{borderRadius: '8px', marginX: 3}}
+                                onClick={() => {signin("bruh", "bruh#chew")
+                                    .then(t => tokenAPI.setToken(t))}}
+                        >Sign In</Button>
                         <Button style={{ borderRadius: '8px',
                                 backgroundColor: "#227173"
                             }} variant={"contained"} size={"large"}>
