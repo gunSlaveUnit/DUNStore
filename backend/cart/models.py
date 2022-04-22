bcc75c088625128320ccd1643a666bec794273ee
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-22 23:23:17 +0300

    Added a cart row model

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class CartRow(models.Model):
+    token = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    slug = models.CharField(max_length=200)
+
