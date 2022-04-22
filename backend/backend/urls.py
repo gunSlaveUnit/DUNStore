68f54705aecfaa1273e856f2197a484b91c7c665
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-22 23:19:55 +0300

    Added a cart urls to the app urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/store/', include('store.urls')),
+    path('api/v1/cart/', include('cart.urls')),
     path('api/v1/auth/', include('djoser.urls')),
     re_path('api/v1/auth/', include('djoser.urls.authtoken')),
 ]
