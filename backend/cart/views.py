13b35f4b40f6ac8c5ba44e23eea1aaf96901d175
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-04-24 11:31:00 +0300

    Added views to get a list of cart rows in user's cart and to add a product

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from cart.models import CartRow
+from cart.serializers import CartRowSerializer
+
+
+class CartRowListAPIView(ListAPIView):
+    serializer_class = CartRowSerializer
+
+    def list(self, request, *args, **kwargs):
+        token = request.query_params.get("token")
+        queryset = CartRow.objects.filter(token=token)
+        serializer = CartRowSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class CartRowCreateAPIView(CreateAPIView):
+    serializer_class = CartRowSerializer
