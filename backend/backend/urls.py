113256c1a99089c3588a76febf047716ad771b3a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-01 17:15:22 +0300

    Added users urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/store/', include('store.urls')),
     path('api/v1/cart/', include('cart.urls')),
+    path('api/v1/users', include('users.urls')),
 ]
