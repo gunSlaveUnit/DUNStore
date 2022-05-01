fdcd750d41572f2ce0e7adbc12a26792e19adb8e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-01 16:56:31 +0300

    Token auth class changed to jwt auth class in default auth classes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,7 +142,7 @@ CORS_ORIGIN_WHITELIST = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
