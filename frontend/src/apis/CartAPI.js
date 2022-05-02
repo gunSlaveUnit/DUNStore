de276dd46ed1b7cc723af58a195dc8267c0e7a04
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-02 17:21:19 +0300

    Cart can display products

--- frontend/src/apis/CartAPI.js
+++ frontend/src/apis/CartAPI.js
@@ -1,5 +1,15 @@
 const URL_BASE = "http://localhost:8000/api/v1/cart/";
 
+export async function list(token) {
+    const requestOptions = {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+    };
+
+    const res = await fetch(URL_BASE + `?token=${token}`, requestOptions);
+    return await res.json()
+}
+
 export async function add(token, category, slug, amount) {
     const requestOptions = {
         method: "POST",
--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import * as CartAPI from "../apis/CartAPI";
+import * as ProductAPI from "../apis/API";
+import {Container, Typography} from "@mui/material";
+import Loader from "./Loader";
+import {useCookies} from "react-cookie";
+import ProductCard from "./ProductCard";
 
 export default function Cart() {
-    return (
-        <div>
+    const [products, setProducts] = React.useState([]);
+    const [cookies, setCookie, removeCookie] = useCookies([]);
+    const [loading, setLoading] = React.useState(true);
 
-        </div>
-    );
+    useEffect(() => {
+        CartAPI.list(cookies["access"])
+            .then(products => {
+                Promise
+                    .all(products
+                        .map(p => ProductAPI
+                            .detail(p.category, p.slug)
+                            .then(r => p.info = r)
+                            .then(() => p)))
+                    .then(results => setProducts(results))
+            })
+            .then(_ => setLoading(false));
+    }, [cookies])
+
+    return (<Container sx={{marginTop: 11}} fixed>
+        {loading && <Loader/>}
+        {products.length ? (<Container fixed sx={{marginTop: 3}} maxWidth={"lg"} style={{alignItems: "center"}}>
+            {products.map(c => <ProductCard key={c.id} group={c.category} card={c.info}/>)}
+        </Container>) : (
+            loading ? null :
+                <Typography variant={"h3"}
+                          align={"center"}
+                          mt={7} style={{color: "#7a9cbc"}}
+                          gutterBottom>
+                    No products in your cart
+                </Typography>)}
+    </Container>);
 }
