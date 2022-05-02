b0ecd41640dcf6126a5df9ad178194b240caaa66
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-02 19:41:40 +0300

    Cart can display order price

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -5,6 +5,7 @@ import {Container, Typography} from "@mui/material";
 import Loader from "./Loader";
 import {useCookies} from "react-cookie";
 import ProductCard from "./ProductCard";
+import CartProduct from "./CartProduct";
 
 export default function Cart() {
     const [products, setProducts] = React.useState([]);
@@ -25,16 +26,33 @@ export default function Cart() {
             .then(_ => setLoading(false));
     }, [cookies])
 
+    const [price, setPrice] = React.useState(0);
+    useEffect(
+        () => {
+            setPrice(products.reduce((price, p) => price + p.info.price, 0));
+        },
+        [products]);
+
     return (<Container sx={{marginTop: 11}} fixed>
         {loading && <Loader/>}
-        {products.length ? (<Container fixed sx={{marginTop: 3}} maxWidth={"lg"} style={{alignItems: "center"}}>
-            {products.map(c => <ProductCard key={c.id} group={c.category} card={c.info}/>)}
-        </Container>) : (
+        {products.length ? (
+            <React.Fragment>
+                 <Typography variant={"h4"}
+                            align={"center"}
+                            mt={7} style={{color: "#7a9cbc"}}
+                            gutterBottom>
+                    {`Order price: ${price}`}&#8381;
+                </Typography>
+                <Container fixed sx={{marginTop: 3}} maxWidth={"lg"} style={{alignItems: "center"}}>
+                    {products.map(c => <CartProduct key={c.id} group={c.category} card={c.info}/>)}
+                </Container>
+            </React.Fragment>
+        ) : (
             loading ? null :
                 <Typography variant={"h3"}
-                          align={"center"}
-                          mt={7} style={{color: "#7a9cbc"}}
-                          gutterBottom>
+                            align={"center"}
+                            mt={7} style={{color: "#7a9cbc"}}
+                            gutterBottom>
                     No products in your cart
                 </Typography>)}
     </Container>);
---
+++ frontend/src/components/CartProduct.js
@@ -0,0 +1,93 @@
+import React, {useEffect} from 'react';
+import {A, navigate} from 'hookrouter';
+import {add} from "../apis/CartAPI";
+import {useCookies} from "react-cookie";
+import {
+    Button,
+    ButtonBase,
+    Link,
+    CardActionArea,
+    CardContent,
+    Grid,
+    Paper,
+    Stack,
+    styled,
+    Typography, Card
+} from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Delete from "./Delete";
+import Update from "./Update";
+
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
+
+export default function CartProduct(props) {
+    const [cookies, setCookie, removeCookie] = useCookies(['token']);
+
+    function handleProductCardTitleClicked() {
+        navigate(`/catalog/${props.group}/detail/${props.card.slug}`)
+    }
+
+    const avoidedServiceFields = ["id", "title", "created_at", "updated_at", "slug", "is_published", "price"];
+
+    return (
+        <Card
+            style={{
+                p: 2,
+                flexGrow: 1,
+                backgroundColor: "#27282b",
+                height: 300,
+                width: 750,
+                padding: 30,
+                marginBottom: 30,
+                borderRadius: "1.8em"
+            }}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm container>
+                    <Grid item xs container direction="column" spacing={2}>
+                        <Grid item xs>
+                            <Link onClick={() => handleProductCardTitleClicked()}>
+                                <Typography gutterBottom variant="h5" sx={{fontWeight: 'bold'}}
+                                            style={{color: "#eceded"}}
+                                            component="div">
+                                    {props.card.title}
+                                </Typography>
+                            </Link>
+                            <Typography variant="body2" style={{color: "#6c6964"}} gutterBottom>
+                                ID: 1030114
+                            </Typography>
+                            {Object.keys(props.card).map(f => {
+                                if (!avoidedServiceFields.includes(f))
+                                    return (
+                                        <Typography variant="body1" key={f} style={{color: "#eceded"}}>
+                                            {f.charAt(0).toUpperCase() + f.slice(1).replace(/_/g, " ") + ": " + props.card[f]}
+                                        </Typography>
+                                    )
+                            })}
+                        </Grid>
+                        <Grid item>
+                            <Typography variant="body2" style={{color: "#6c6964"}}>
+                                Available in
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item>
+                        <Stack>
+                            <Typography variant="h5" sx={{fontWeight: 'bold'}} style={{color: "#eceded"}}
+                                        component="div" align={"right"}>
+                                {props.card.price}&#8381;
+                            </Typography>
+
+                            <Delete what={props.group} product={props.card}/>
+                        </Stack>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Card>
+    );
+}
