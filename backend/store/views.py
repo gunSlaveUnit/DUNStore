b5eff45c0d2df1ad30c32a6d439812721202d7f4
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-07 01:16:23 +0300

    Added permissions to viewsets. List and retrieve - IsAuthenticatedOrReadOnly, others - IsAdminUser

--- backend/store/views.py
+++ backend/store/views.py
@@ -1,13 +1,26 @@
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Category, Build, StorageDevice, VideoCard, Motherboard, Processor, RAM, PowerUnit
-from store.serializers import CategorySerializer, BuildSerializer, StorageDeviceSerializer, VideoCardSerializer, MotherboardSerializer, ProcessorSerializer, RAMSerializer, PowerUnitSerializer
+from store.serializers import CategorySerializer, BuildSerializer, StorageDeviceSerializer, VideoCardSerializer, \
+    MotherboardSerializer, ProcessorSerializer, RAMSerializer, PowerUnitSerializer
 
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'slug'
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        """
+            Instantiates and returns the list of permissions that this view requires.
+            """
+        if self.action in ('list', 'retrieve'):
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
 class BuildViewSet(ModelViewSet):
@@ -15,11 +28,31 @@ class BuildViewSet(ModelViewSet):
     serializer_class = BuildSerializer
     lookup_field = 'slug'
 
+    def get_permissions(self):
+        """
+            Instantiates and returns the list of permissions that this view requires.
+            """
+        if self.action in ('list', 'retrieve'):
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 
 class StorageDeviceViewSet(ModelViewSet):
     queryset = StorageDevice.objects.filter(is_published=True)
     serializer_class = StorageDeviceSerializer
     lookup_field = 'slug'
+
+    def get_permissions(self):
+        """
+            Instantiates and returns the list of permissions that this view requires.
+            """
+        if self.action in ('list', 'retrieve'):
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
 class VideoCardViewSet(ModelViewSet):
@@ -27,11 +60,31 @@ class VideoCardViewSet(ModelViewSet):
     serializer_class = VideoCardSerializer
     lookup_field = 'slug'
 
+    def get_permissions(self):
+        """
+            Instantiates and returns the list of permissions that this view requires.
+            """
+        if self.action in ('list', 'retrieve'):
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 
 class MotherboardViewSet(ModelViewSet):
     queryset = Motherboard.objects.filter(is_published=True)
     serializer_class = MotherboardSerializer
     lookup_field = 'slug'
+
+    def get_permissions(self):
+        """
+            Instantiates and returns the list of permissions that this view requires.
+            """
+        if self.action in ('list', 'retrieve'):
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
 
 
 class ProcessorViewSet(ModelViewSet):
@@ -39,14 +92,44 @@ class ProcessorViewSet(ModelViewSet):
     serializer_class = ProcessorSerializer
     lookup_field = 'slug'
 
+    def get_permissions(self):
+        """
+            Instantiates and returns the list of permissions that this view requires.
+            """
+        if self.action in ('list', 'retrieve'):
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 
 class RAMViewSet(ModelViewSet):
     queryset = RAM.objects.filter(is_published=True)
     serializer_class = RAMSerializer
     lookup_field = 'slug'
 
+    def get_permissions(self):
+        """
+            Instantiates and returns the list of permissions that this view requires.
+            """
+        if self.action in ('list', 'retrieve'):
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
 
 class PowerUnitViewSet(ModelViewSet):
     queryset = PowerUnit.objects.filter(is_published=True)
     serializer_class = PowerUnitSerializer
     lookup_field = 'slug'
+
+    def get_permissions(self):
+        """
+            Instantiates and returns the list of permissions that this view requires.
+            """
+        if self.action in ('list', 'retrieve'):
+            permission_classes = [IsAuthenticatedOrReadOnly]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
