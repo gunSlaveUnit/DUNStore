955cfdef7a0b55996df1be9d5f189b150fac3b4d
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-08 00:46:03 +0300

    Updated make_payment function. Now it returns a dict of status and message. Updated docs for this function

--- backend/order/services.py
+++ backend/order/services.py
@@ -1,11 +1,25 @@
 import random
 
 
-def make_payment():
+def make_payment(payment_info: dict) -> dict:
     """
     The function is a stub.
-    It assumes data encryption and interaction with the banking service
-    With a probability of 90 percent, the payment will be successful
+    It assumes data encryption and interaction with the banking service.
+    With a probability of 90 percent, the payment will be successful.
+    Possible statuses: 200 - OK, 400 - error
+    :param payment_info: a dict of payment information:
+        {"cardNumber": ...,
+        "validityMonth": ...,
+        "validityYear": ...,
+        "cvv_cvc": ...}
+    :return: dict {"status": ..., "message": ...}
     """
+    if not isinstance(payment_info, dict):
+        return {'status': '400', 'message': 'Invalid payment data provided. It must be a dictionary'}
+
     is_payment_successful = random.randint(0, 10)
-    return True if is_payment_successful < 8 else False
+
+    result = {'status': '200', 'message': 'The payment was successful'} if is_payment_successful < 8 else {
+        'status': '400', 'message': 'Something was wrong'}
+
+    return result
