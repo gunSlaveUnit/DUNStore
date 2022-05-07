3d70c8372075b86c303ed01553fa6de53ca99338
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-08 00:14:08 +0300

    Added a function to pay

--- frontend/src/apis/OrderAPI.jsx
+++ frontend/src/apis/OrderAPI.jsx
@@ -13,3 +13,17 @@ export async function create(what, how, token) {
     const res = await fetch(URL_BASE + what + "/", requestOptions);
     return await res.json()
 }
+
+export async function pay(cardPaymentInfo, token) {
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(cardPaymentInfo)
+    };
+
+    const res = await fetch(URL_BASE + "payment/" , requestOptions);
+    return await res.json()
+}
