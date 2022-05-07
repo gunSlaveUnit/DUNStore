1988d8091ac03f7e329ce37005f460a061a9a234
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-08 01:01:37 +0300

    Added a payment url

--- backend/order/urls.py
+++ backend/order/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from order.views import DeliveryAddressViewSet
+from order.views import DeliveryAddressViewSet, payment
 
 router = routers.SimpleRouter()
 router.register(r'addresses', DeliveryAddressViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('payment/', payment)
 ]
