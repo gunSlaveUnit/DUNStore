c971bcf50bdab049922e6423cb2dc467534b083e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-08 01:01:29 +0300

    Added a payment view

--- backend/order/views.py
+++ backend/order/views.py
@@ -1,8 +1,12 @@
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from order.models import DeliveryAddress
 from order.serializers import DeliveryAddressSerializer
+from order.services import make_payment
 
 
 class DeliveryAddressViewSet(ModelViewSet):
@@ -19,3 +23,14 @@ class DeliveryAddressViewSet(ModelViewSet):
         else:
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def payment(request):
+    payment_data = request.data
+    if not payment_data:
+        return Response(data={"data": "Payment info not provided"}, status=status.HTTP_400_BAD_REQUEST)
+
+    pay_result = make_payment(payment_data)
+    return Response(data={"message": pay_result["message"]}, status=pay_result["status"])
