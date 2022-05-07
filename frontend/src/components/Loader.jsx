a9d80350efe417aa91d8073c38bc3e6151fe379a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-07 21:01:28 +0300

    Added text "Loading ..." after progress

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import {CircularProgress, Stack} from "@mui/material";
+import {CircularProgress, Stack, Typography} from "@mui/material";
 
 const Loader = () => {
     return (
         <Stack alignItems="center" mt={7}>
             <CircularProgress size={"4rem"} disableShrink
-                              style={{color: "#ae718f"}}/>
+                              style={{color: "#7a9cbc"}}/>
+            <Typography variant={"h5"}
+                                align={"center"}
+                                mt={1} style={{color: "#7a9cbc"}}
+                                gutterBottom
+                    >
+                        Loading ...
+                    </Typography>
         </Stack>
     )
 }
