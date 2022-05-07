8a12205df0da3eef2aab6311e3fb4664d950b7c6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-07 19:00:03 +0300

    Added DeliveryAddress model

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class DeliveryAddress(models.Model):
+    city = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    house = models.CharField(max_length=100)
+    building = models.CharField(max_length=100, null=True)
+    frame = models.CharField(max_length=100, null=True)
+    entrance = models.CharField(max_length=100, null=True)
+    floor = models.CharField(max_length=100, null=True)
