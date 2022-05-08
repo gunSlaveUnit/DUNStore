cda2a355d43ca40cf5f7155610509bbec6ce2c90
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-08 14:17:55 +0300

    Added OrderItem model

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class ObtainWay(models.Model):
     title = models.CharField(max_length=50)
@@ -17,3 +19,18 @@ class DeliveryAddress(models.Model):
     frame = models.CharField(max_length=100, null=True)
     entrance = models.CharField(max_length=100, null=True)
     floor = models.CharField(max_length=100, null=True)
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, models.DO_NOTHING)
+    receiver_name = models.CharField(max_length=100)
+    receiver_surname = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    alt_phone = models.CharField(max_length=20)
+    obtain_method = models.ForeignKey(ObtainWay, models.DO_NOTHING)
+    address = models.ForeignKey(DeliveryAddress, models.DO_NOTHING)
+    status = models.ForeignKey(Status, models.DO_NOTHING)
+    is_paid = models.BooleanField()
+    category_slug = models.CharField(max_length=100)
+    product_slug = models.CharField(max_length=100)
+    cost = models.IntegerField()
