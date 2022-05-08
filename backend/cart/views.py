05afd789bd40142e49d23ae53f5650ae4f90cfea
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-09 02:56:08 +0300

    Changed create method: when item is already exist, it adds amount, not makes new object

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -16,3 +16,29 @@ class CartRowViewSet(ModelViewSet):
         queryset = CartRow.objects.filter(token=token)
         serializer = CartRowSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        auth_header = request.headers.get("Authorization")
+        token = auth_header[auth_header.find(" ")+1:]
+
+        if token is None:
+            return Response("Token not provided")
+
+        new_data = request.data
+        try:
+            queryset = CartRow.objects.get(token=token, category=new_data["category"], slug=new_data["slug"])
+
+            queryset.amount += new_data["amount"]
+            queryset.save()
+            serializer = CartRowSerializer(queryset, many=False)
+
+            return Response(serializer.data)
+        except CartRow.DoesNotExist:
+            new = CartRow.objects.create(token=new_data["token"],
+                                         category=new_data["category"],
+                                         slug=new_data["slug"],
+                                         amount=new_data["amount"])
+            new.save()
+            serializer = CartRowSerializer(new, many=False)
+
+            return Response(serializer.data)
