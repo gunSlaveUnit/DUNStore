6ec28ec11781f005a2f5b7547f97c3bf64879a40
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-08 14:18:36 +0300

    Registered OrderItem model

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from order.models import ObtainWay, Status, DeliveryAddress
+from order.models import ObtainWay, Status, DeliveryAddress, OrderItem
 
 admin.site.register(ObtainWay)
 admin.site.register(Status)
 admin.site.register(DeliveryAddress)
+admin.site.register(OrderItem)
