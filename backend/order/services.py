71cdb751e1d2adaaa72f4a9c5611672620659ab6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-09 11:34:14 +0300

    Added a function to send sms notification (stub)

--- backend/order/services.py
+++ backend/order/services.py
@@ -25,3 +25,22 @@ def make_payment(payment_info: dict) -> dict:
         'status': status.HTTP_400_BAD_REQUEST, 'message': 'Something was wrong'}
 
     return result
+
+
+def send_sms_notification(sms_info: dict) -> dict:
+    if not isinstance(sms_info, dict):
+        return {'status': status.HTTP_400_BAD_REQUEST,
+                'message': 'Invalid sms data provided. It must be a dictionary'}
+
+    try:
+        request = None
+        # makes a request
+    except ConnectionError as ex:
+        pass
+
+    is_sms_successful = random.randint(0, 10)
+
+    result = {'status': status.HTTP_200_OK, 'message': 'Sent successfully'} if is_sms_successful < 8 else {
+        'status': status.HTTP_400_BAD_REQUEST, 'message': 'Something was wrong'}
+
+    return result
