f9d9302cb1d6e9df8aa00ae3dab2bf54c2d7fd8a
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-05-10 09:30:45 +0300

    Added is_store field to delivery address

--- backend/order/models.py
+++ backend/order/models.py
@@ -15,10 +15,11 @@ class DeliveryAddress(models.Model):
     city = models.CharField(max_length=100)
     street = models.CharField(max_length=100)
     house = models.CharField(max_length=100)
-    building = models.CharField(max_length=100, null=True)
-    frame = models.CharField(max_length=100, null=True)
-    entrance = models.CharField(max_length=100, null=True)
-    floor = models.CharField(max_length=100, null=True)
+    building = models.CharField(max_length=100, null=True, blank=True)
+    frame = models.CharField(max_length=100, null=True, blank=True)
+    entrance = models.CharField(max_length=100, null=True, blank=True)
+    floor = models.CharField(max_length=100, null=True, blank=True)
+    is_store = models.BooleanField()
 
 
 class OrderItem(models.Model):
